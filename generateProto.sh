63cdb9667ea1276226b2481ab48f716fd9ca66ee
Martijn van Maasakkers <martijn@newestindustry.nl>
2018-06-05 16:39:50 +0200

    Added gateway

--- generateProto.sh
+++ generateProto.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-protoc -I=../proto/v1 -I=/usr/local/include -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,import_path=api_v1:./service/v1 ../proto/v1/*.proto
+protoc -I=/usr/local/include \
+    -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I=../proto/v1 \
+    --go_out=plugins=grpc,import_path=api_v1:./service/v1 \
+    ../proto/v1/*.proto
+
+protoc -I/usr/local/include \
+    -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I=../proto/v1 \
+    --grpc-gateway_out=logtostderr=true:./service/v1/gateway \
+    ../proto/v1/*.proto
